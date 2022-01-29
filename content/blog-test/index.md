df934e97e3e4e9e32c4f28accc818606fab86d85
가즈아 <wjsdydxo45@naver.com>
2022-01-30 01:38:25 +0900

    test2

---
+++ content/blog-test/index.md
@@ -0,0 +1,8 @@
+---
+emoji: 🧢
+title: test
+date: '2021-03-22 23:00:00'
+author: 줌코딩
+tags: blog gatsby theme 개츠비 테마
+categories: 블로그
+---
--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -74,9 +74,9 @@ module.exports = {
       {
         title: '항해99 실전프로젝트 - 생드백',
         description:
-          `Think와 ThandBag의 합성어 생드백이라는 이름으로, 일상 생활속에서 받은 스트레스, 고민을 샌드백을 때리면서 재미있게 풀 수 있는 스트레스 해소 서비스를 기획, 개발, 배포하였습니다. ( Front-End 3명, Back-End 2명 )`,
+          `Think와 ThandBag의 합성어 생드백이라는 이름으로, 일상 생활속에서 받은 스트레스, 고민을 샌드백을 때리면서 재미있게 풀 수 있는 스트레스 해소 서비스를 기획, 개발, 배포하였습니다.`,
         techStack: ['react', 'redux', 'pwa', 'amazon s3'],
-        thumbnailUrl: 'thandbag.png',
+        thumbnailUrl: 'thandbag_play.gif',
         links: {
           post: '/gatsby-starter-zoomkoding-introduction',
           github: 'https://github.com/thandbag/thandbag_FE',
