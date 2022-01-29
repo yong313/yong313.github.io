c678a01f022d1e50f464e5e8a6b37604c5b8d0f3
가즈아 <wjsdydxo45@naver.com>
2022-01-30 00:16:34 +0900

    test

--- src/components/page-footer/index.js
+++ src/components/page-footer/index.js
@@ -3,18 +3,17 @@ import './style.scss';
 
 function PageFooter({ author, githubUrl }) {
   return (
-    null
-    // <footer className="page-footer-wrapper">
-    //   <p className="page-footer">
-    //     © {new Date().getFullYear()}
-    //     &nbsp;
-    //     <a href={githubUrl}>{author}</a>
-    //     &nbsp;powered by
-    //     <a href="https://github.com/zoomKoding/zoomkoding-gatsby-blog">
-    //       &nbsp;zoomkoding
-    //     </a>
-    //   </p>
-    // </footer>
+    <footer className="page-footer-wrapper">
+      <p className="page-footer">
+        © {new Date().getFullYear()}
+        &nbsp;
+        <a href={githubUrl}>{author}</a>
+        &nbsp;powered by
+        <a href="https://github.com/zoomKoding/zoomkoding-gatsby-blog">
+          &nbsp;zoomkoding
+        </a>
+      </p>
+    </footer>
   );
 }
 
