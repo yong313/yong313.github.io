23a70d9f32131d07cab8915935c28dbb090eaa8f
가즈아 <wjsdydxo45@naver.com>
2022-01-30 00:09:36 +0900

    blog test2

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -14,7 +14,7 @@ module.exports = {
     name: `전용태`,
     bio: {
       role: `개발자`,
-      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
+      description: ['새로운 도약을 꿈꾸는', '열정적인 자세의', '이로운 것을 만드는'],
       thumbnail: 'main.gif', // Path to the image in the 'asset' folder
     },
     social: {
--- src/components/page-footer/index.js
+++ src/components/page-footer/index.js
@@ -3,17 +3,18 @@ import './style.scss';
 
 function PageFooter({ author, githubUrl }) {
   return (
-    <footer className="page-footer-wrapper">
-      <p className="page-footer">
-        © {new Date().getFullYear()}
-        &nbsp;
-        <a href={githubUrl}>{author}</a>
-        &nbsp;powered by
-        <a href="https://github.com/zoomKoding/zoomkoding-gatsby-blog">
-          &nbsp;zoomkoding-gatsby-blog
-        </a>
-      </p>
-    </footer>
+    null
+    // <footer className="page-footer-wrapper">
+    //   <p className="page-footer">
+    //     © {new Date().getFullYear()}
+    //     &nbsp;
+    //     <a href={githubUrl}>{author}</a>
+    //     &nbsp;powered by
+    //     <a href="https://github.com/zoomKoding/zoomkoding-gatsby-blog">
+    //       &nbsp;zoomkoding
+    //     </a>
+    //   </p>
+    // </footer>
   );
 }
 
